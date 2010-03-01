d812ccf844d09147df5a353b0a495c9a3a05bffc
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-03-01 08:51:24 +0000

    [FIX] skin export: check for errors

--- actions/ExportAction.class.php
+++ actions/ExportAction.class.php
@@ -44,18 +44,41 @@ class skin_ExportAction extends f_action_BaseAction
 		}
 		$skinDefFile = gzcompress(serialize($skinDefContent));
 		$filename = str_replace('°', '_', f_util_FileUtils::normalizeFilename($document->getLabel()));
-		f_util_FileUtils::mkdir(TMP_PATH);
-		$filePath = TMP_PATH . DIRECTORY_SEPARATOR . "skin-" . mt_rand() . $filename . ".zip";
-		
+		$filePath = tempnam(null, "skinzip");
 		$zip = new ZipArchive();
-		$zip->open($filePath, ZipArchive::CREATE);
-		$zip->addFromString($filename . ".skin", $skinDefFile);
+		$zipRes = $zip->open($filePath, ZipArchive::CREATE | ZIPARCHIVE::OVERWRITE);
+		if ($zipRes !== true)
+		{
+			switch ($zipRes)
+			{
+				case ZIPARCHIVE::ER_EXISTS : $resString = "ER_EXISTS"; break;
+				case ZIPARCHIVE::ER_INCONS : $resString = "ER_INCONS"; break;
+				case ZIPARCHIVE::ER_INVAL : $resString = "ER_INVAL"; break;
+				case ZIPARCHIVE::ER_MEMORY : $resString = "ER_MEMORY"; break;
+				case ZIPARCHIVE::ER_NOENT : $resString = "ER_NOENT"; break;
+				case ZIPARCHIVE::ER_NOZIP : $resString = "ER_NOZIP"; break;
+				case ZIPARCHIVE::ER_OPEN : $resString = "ER_OPEN"; break;
+				case ZIPARCHIVE::ER_READ : $resString = "ER_READ"; break;
+				case ZIPARCHIVE::ER_SEEK : $resString = "ER_SEEK"; break;
+				default: $resString = "Unknown error : ".var_export($zipRes, true);
+			}
+			throw new Exception("Unable to create zip archive $filePath: ".$resString);	
+		}
+		if (!$zip->addFromString($filename . ".skin", $skinDefFile))
+		{
+			throw new Exception("Unable to add skin def file to zip archive"); 
+		}
 		foreach ($skinFiles as $id => $path)
 		{
-			$zip->addFile($path, $id . f_util_FileUtils::getFileExtension($path, true));
+			if (!$zip->addFile($path, $id . f_util_FileUtils::getFileExtension($path, true)))
+			{
+				throw new Exception("Unable to add $path to zip archive");
+			}
 		}
-		$res = $zip->close();
-		Framework::fatal("-->" . file_get_contents($filePath));
+		if (!$zip->close())
+		{
+			throw new Exception("Could not close zip archive");
+		}
 		
 		$headers[] = 'Cache-Control: public, must-revalidate';
 		$headers[] = 'Pragma: hack';
