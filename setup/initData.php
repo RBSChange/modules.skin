08cd40a439f849bbf9cc5525a78e80236c684923
Gaël PORT <gael.port@rbs.fr>
2012-09-20 09:14:23 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,15 +3,6 @@ class skin_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('skin', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+		$this->executeModuleScript('init.xml');
 	}
 }
